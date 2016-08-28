bb043e0fce221edb817c0bf6ff0e9cf0fa920ed8
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-08-28 19:28:44 -0300

    creating a maven project

---
+++ src/main/java/br/com/urban/sandbox/maven/App.java
@@ -0,0 +1,13 @@
+package br.com.urban.sandbox.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
