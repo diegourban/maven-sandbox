21cdda9cd586b28870aa99f114b51e39b4f75ca2
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-08-28 21:05:29 -0300

    Maven with eclipse

---
+++ src/main/java/br/com/urban/sandbox/maven/Produto.java
@@ -0,0 +1,14 @@
+package br.com.urban.sandbox.maven;
+
+public class Produto {
+
+	private final String nome;
+	private final Double preco;
+
+	public Produto(String nome, Double preco) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+}
